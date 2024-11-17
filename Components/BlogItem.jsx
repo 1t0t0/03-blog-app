39a3fed828cd2feb2d1510b397ecd97f94f44fce
Toto <toto@Totos-MacBook-Pro.local>
2024-11-17 08:25:08 +0700

    change blogitem  hover color from black to blue

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const BlogItem = ({ title, description, category, image, id }) => {
     return (
-        <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+        <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#00DBFF]'>
             <Link href={`/blogs/${id}`}>
                 <Image src={image} alt='' width={400} height={400} className='border-black' />
             </Link>
